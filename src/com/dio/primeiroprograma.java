3f01b608150e02a4acc9587664337636135fa712
Klaus-Cloud <thunder20161@hotmail.com>
2022-05-13 09:55:21 -0300

    First commit

---
+++ src/com/dio/primeiroprograma.java
@@ -0,0 +1,19 @@
+package com.dio;
+
+import com.dio.model.Gato;
+
+public class primeiroprograma {
+
+	public static void main(String[] args) {
+		
+		Gato gato = new Gato();
+		System.out.println(gato);
+
+	}
+
+}
+
+class Livros {
+	private String nome;
+	private String npag;
+}
